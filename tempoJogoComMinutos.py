fc97d21bd3820e722f05b54fc0dc66bfbfc43cbc
Augusto Machado <augustomachadoramos@gmail.com>
2019-04-18 19:18:14 -0300

    Create tempoJogoComMinutos.py

---
+++ tempoJogoComMinutos.py
@@ -0,0 +1,32 @@
+h1, min1, h2, min2 = input().split()
+
+h1 = int(h1)
+min1 = int(min1)
+h2 = int(h2)
+min2 = int(min2)
+
+horas = 0
+minutos = 0
+if h1 == h2:
+    if min1 == min2:
+        print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
+    else:
+        if min1 > min2:
+            horas = 23
+            minutos = (60 - min1) + min2
+        else:
+            horas = h1 - h2
+            minutos = min2 - min1
+else:
+    if min1 > min2:
+        minutos = (60 - min1) + min2
+    else:
+        minutos = min2 - min1
+
+    if h1 > h2:
+        horas = (24 - h1) + h2
+    elif h2 > h1:
+        if min1 < min2 or min1 == min2:
+            horas = h2 - h1
+    
+print("O JOGO DUROU {} HORA(S) E {} MINUTO(S)".format(horas, minutos))
