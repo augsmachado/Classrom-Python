71601a65cd59de39907c295f92436e949c9ab61a
Augusto Machado <augustomachadoramos@gmail.com>
2019-04-18 15:25:24 -0300

    Example: add many variables on 1 line

--- Input_function.py
+++ Input_function.py
@@ -1,3 +1,12 @@
-﻿print("Hi, I'm Python. What's your name?")
+#Entrada de texto
+print("Hi, I'm Python. What's your name?")
 name = input()
 print("Hello,",name,"- nice to meet you!")
+
+#Entrada de múltiplas variáveis na mesma linha
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+print("Resultado: {:.2}".format(a/b))
